3be1558517923919c3c84979c49d8740da0e63eb
fatima-mehmood <110164660+fatima-mehmood@users.noreply.github.com>
2023-10-12 10:21:37 +0500

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.get('/api/s3image', (req, res) => {
 });
 
 // Start the server
-const PORT = 3000;
+const PORT = 80;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
